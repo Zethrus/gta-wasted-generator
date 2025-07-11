f10dc552fb8e513ed50dd39176f793360f25fd6b
Victor Lane <contact@zethr.us>
2025-07-10 23:42:43 -0600

    Fix ti!

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 // --- Multer Setup for Image Uploads ---
 const upload = multer({
   storage: multer.memoryStorage(), // Process image in memory
-  limits: { fileSize: 10 * 1024 * 1024 }, // 10MB limit
+  limits: { fileSize: 10 * 1024 * 1024 } // 10 MB
   fileFilter: (req, file, cb) => {
     if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
       cb(null, true);
